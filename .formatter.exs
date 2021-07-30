af3551ccd2a0924609ed69ca6412e12991aa73db
Arijan B <arijan.berisa@verybigthings.com>
2021-07-30 14:59:23 +0200

    Add .formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,9 @@
+[
+  import_deps: [:phoenix, :ecto, :ecto_sql, :ecto_enum, :absinthe, :surface],
+  inputs: [
+    "*.{ex,exs}",
+    "priv/*/seeds.exs",
+    "{config,lib,test}/**/*.{ex,exs}",
+    "priv/*/migrations/*.{ex,exs}"
+  ]
+]
