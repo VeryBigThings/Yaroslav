1ff27548491dc6fe30aa49a67f05a9f69d707940
Yaroslav <kseofon@gmail.com>
2021-08-20 12:52:25 +0300

    Sorting exercises 2

---
+++ vbtfriends/lib/vbtfriends_web/live/sort_live.ex
@@ -0,0 +1,112 @@
+defmodule VbtfriendsWeb.SortLive do
+  use VbtfriendsWeb, :live_view
+
+  alias Vbtfriends.Donations
+
+  def mount(_params, _session, socket) do
+    {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _url, socket) do
+    #page = String.to_integer(params["page"] || "1")
+    #per_page = String.to_integer(params["per_page"] || "5")
+    page = param_to_integer(params["page"], 1)
+per_page = param_to_integer(params["per_page"], 5)
+
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    paginate_options = %{page: page, per_page: per_page}
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+
+    donations =
+      Donations.list_donations(
+        paginate: paginate_options,
+        sort: sort_options
+      )
+
+    socket =
+      assign(socket,
+        options: Map.merge(paginate_options, sort_options),
+        donations: donations
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_event("select-per-page", %{"per-page" => per_page}, socket) do
+    per_page = String.to_integer(per_page)
+
+    socket =
+      push_patch(socket,
+        to:
+          Routes.live_path(
+            socket,
+            __MODULE__,
+            page: socket.assigns.options.page,
+            per_page: per_page,
+            sort_by: socket.assigns.options.sort_by,
+            sort_order: socket.assigns.options.sort_order
+          )
+      )
+
+    {:noreply, socket}
+  end
+
+  defp expires_class(donation) do
+    if Donations.almost_expired?(donation), do: "eat-now", else: "fresh"
+  end
+
+  defp pagination_link(socket, text, page, options, class) do
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          page: page,
+          per_page: options.per_page,
+          sort_by: options.sort_by,
+          sort_order: options.sort_order
+        ),
+      class: class
+    )
+  end
+
+  defp sort_link(socket, text, sort_by, options) do
+    text =
+      if sort_by == options.sort_by do
+        text <> emoji(options.sort_order)
+      else
+        text
+      end
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          sort_by: sort_by,
+          sort_order: toggle_sort_order(options.sort_order),
+          page: options.page,
+          per_page: options.per_page
+        )
+    )
+  end
+
+  defp toggle_sort_order(:asc), do: :desc
+  defp toggle_sort_order(:desc), do: :asc
+
+  defp emoji(:asc), do: "👇"
+  defp emoji(:desc), do: "👆"
+
+  defp param_to_integer(nil, default_value), do: default_value
+  defp param_to_integer(param, default_value) do
+    case Integer.parse(param) do
+      {number, _} ->
+        number
+
+      :error ->
+        default_value
+    end
+  end
+end
